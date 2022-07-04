3a7416e62205dbe7a9307d7ec167e3cce25313db
ikhwanulmn <33983306+ikhwanulmn@users.noreply.github.com>
2022-07-04 22:11:11 +0700

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'https://sage-beijinho-6a893c.netlify.app';
 
     /**
      * --------------------------------------------------------------------------
